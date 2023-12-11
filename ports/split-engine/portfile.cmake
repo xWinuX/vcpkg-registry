b62a33bbd4f108b0c6d8adeb78f427a89d52554c
WinuX <37900754+xWinuX@users.noreply.github.com>
2023-12-12 00:01:44 +0100

    debug portfile

--- ports/split-engine/portfile.cmake
+++ ports/split-engine/portfile.cmake
@@ -6,12 +6,6 @@ vcpkg_from_github(
         HEAD_REF dev
 )
 
-vcpkg_check_features(
-        OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-        FEATURES
-        headless HEADLESS
-)
-
 vcpkg_configure_cmake(
         SOURCE_PATH ${SOURCE_PATH}
         PREFER_NINJA
@@ -20,22 +14,12 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
+
 vcpkg_fixup_cmake_targets()
 
-file(REMOVE_RECURSE
-        ${CURRENT_PACKAGES_DIR}/debug/include
-        ${CURRENT_PACKAGES_DIR}/debug/share
-)
-
-file(INSTALL ${SOURCE_PATH}/include DESTINATION ${CURRENT_PACKAGES_DIR}/include
-        FILES_MATCHING
-        PATTERN "*.h"
-        PATTERN "*.hpp"
-)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file(
         INSTALL "${SOURCE_PATH}/LICENSE"
         DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
         RENAME copyright)
-
-file(INSTALL ${SOURCE_PATH}/res DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
