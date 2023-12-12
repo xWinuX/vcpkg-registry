9917d597f4dde7da3cfc02340d118e1cc34761db
WinuX <37900754+xWinuX@users.noreply.github.com>
2023-12-12 16:39:28 +0100

    Update portfile.cmake

--- ports/split-engine/portfile.cmake
+++ ports/split-engine/portfile.cmake
@@ -13,8 +13,7 @@ vcpkg_configure_cmake(
 
 vcpkg_build_cmake(TARGET SplitEngine)
 
+vcpkg_copy_pdbs()
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/box2d)
-
-vcpkg_copy_pdbs()
