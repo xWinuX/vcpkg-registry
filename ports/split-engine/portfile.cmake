6b654be59f33b3eafb85a3e423c77dc36352eca0
WinuX <37900754+xWinuX@users.noreply.github.com>
2023-12-13 10:22:50 +0100

    Update portfile.cmake

--- ports/split-engine/portfile.cmake
+++ ports/split-engine/portfile.cmake
@@ -1,7 +1,9 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO xWinuX/SplitEngine
-        REF 2359e6d37d21176c8b0b673f03487d5ff0911117
+        REF 642e99adb60d9cb3a8ca2e8c78ea39811841acae
         SHA512 38f82ebf441954839d411c3f3657a1a281e236f8150beea2a41557c22357be66adde3174009f7a80cc820a478ed0d7f978db9d22d96100120026360f89bfbf04
         HEAD_REF dev
 )
@@ -16,6 +18,7 @@ vcpkg_cmake_install()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_cmake_config_fixup(
+        CONFIG_PATH lib/cmake/SplitEngine
         PACKAGE_NAME SplitEngine
 )
 
