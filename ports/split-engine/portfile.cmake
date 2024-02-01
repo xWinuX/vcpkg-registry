03d87401560056f2c78dae41395f451849b02ea9
WinuX <37900754+xWinuX@users.noreply.github.com>
2024-02-01 20:36:49 +0100

    Update portfile.cmake

--- ports/split-engine/portfile.cmake
+++ ports/split-engine/portfile.cmake
@@ -6,27 +6,30 @@ find_program(GIT git)
 set(GIT_URL "https://github.com/xWinuX/SplitEngine.git")
 set(GIT_REV "ab2a2ed7eb077ab8a7db429e1bc0d974ef038a2f")
 
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${PORT})
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src)
 
-
-if(NOT EXISTS "${SOURCE_PATH}/.git")
-    message(STATUS "Cloning and fetching submodules")
+if (NOT EXISTS "${DOWNLOADS}/SplitEngine.git")
+    message(STATUS "Cloning")
     vcpkg_execute_required_process(
-            COMMAND ${GIT} clone --recurse-submodules ${GIT_URL} ${SOURCE_PATH}
-            WORKING_DIRECTORY ${SOURCE_PATH}
+            COMMAND ${GIT} clone --recurse-submodules ${GIT_URL} ${DOWNLOADS}/SplitEngine.git
+            WORKING_DIRECTORY ${DOWNLOADS}
             LOGNAME clone
     )
+endif ()
+message(STATUS "Cloning done")
 
+if (NOT EXISTS "${CURRENT_BUILDTREES_DIR}/src/.git")
     message(STATUS "Checkout revision ${GIT_REV}")
     vcpkg_execute_required_process(
             COMMAND ${GIT} checkout ${GIT_REV}
-            WORKING_DIRECTORY ${SOURCE_PATH}
+            WORKING_DIRECTORY ${DOWNLOADS}/SplitEngine.git
             LOGNAME checkout
     )
-endif()
+endif ()
+message(STATUS "Checkout revision done")
 
 vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH}
+        SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src
         PREFER_NINJA
 )
 
@@ -42,6 +45,6 @@ vcpkg_cmake_config_fixup(
 vcpkg_copy_pdbs()
 
 file(
-        INSTALL "${SOURCE_PATH}/LICENSE.md"
+        INSTALL "${CURRENT_BUILDTREES_DIR}/src/LICENSE.md"
         DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
         RENAME copyright)
