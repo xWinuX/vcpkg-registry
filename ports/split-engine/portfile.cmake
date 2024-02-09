fa6c09524c8d53ff952ffe0d799cc712ccd8f12b
WinuX <37900754+xWinuX@users.noreply.github.com>
2024-02-09 05:07:17 +0100

    Update portfile.cmake

--- ports/split-engine/portfile.cmake
+++ ports/split-engine/portfile.cmake
@@ -4,7 +4,7 @@ include(vcpkg_common_functions)
 find_program(GIT git)
 
 set(GIT_URL "https://github.com/xWinuX/SplitEngine.git")
-set(GIT_REV "9ffd370bb76fd53fa4fe80fabf87b0453f9c56e6")
+set(GIT_REV "35270ac70fcdd56aabf4e656e5b903602047cb18")
 
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${PORT})
 
